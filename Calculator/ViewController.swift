09121e8729629f2cddae75bc148623e7565e65c4
Ivan Biriukov <Grottt0@mail.ru>
2023-05-14 01:27:06 +0300

    remove storyboard

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by иван Бирюков on 14.05.2023.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
